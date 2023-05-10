5e9cb6a3e060fc76e9512b5b3ba21af211814838
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-05-10 16:39:07 +0600

    copy data show

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,9 +9,17 @@ const Banner = () => {
             Banner
             <h2>New Added Coffee: {coffees.length}</h2>
             {/* map all coffes data  */}
+            <div className='container mx-auto'>
+            <div className='text-center'>
+            <h1 className='text-3xl font-[Rancho]'>Our Popular Coffee</h1>
+            <button>Add Coffee</button>
+            </div>
+            <div className="grid md:grid-cols-2 gap-4 ">
             {
                 coffees.map((coffee) =><PopularProduct key={coffee._id} coffee={coffee}></PopularProduct>)
             }
+            </div>
+            </div>
         </div>
     );
 };
--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -3,7 +3,36 @@ import React from 'react';
 const PopularProduct = ({coffee}) => {
     return (
         <div>
-            {coffee.name}
+            <div >
+      <div className="bg-[#F4F3F0] p-4 shadow-md rounded-lg ">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <img
+              src={coffee.photo}
+              alt="Card Image"
+              className="w-full h-auto rounded"
+            />
+          </div>
+          <div className="col-span-1 flex flex-col items-center justify-center">
+            <p className="text-lg font-semibold">Name: {coffee.name}</p>
+            <p className="text-lg font-semibold">Chef: {coffee.chef}</p>
+            <p className="text-lg font-semibold">Category: {coffee.category}</p>
+          </div>
+          <div className="col-span-1 flex flex-col justify-center">
+            <button className="py-2 px-4 mb-2">
+             <img src="https://i.ibb.co/n1qkQJP/view-Button.png" alt="" />
+            </button>
+            <button className="py-2 px-4 mb-2">
+              <img src="https://i.ibb.co/G08dvDK/edit-button.png" alt="" />
+            </button>
+            <button className="py-2 px-4">
+              <img src="https://i.ibb.co/b1ngMDt/delete-button.png" alt="" />
+            </button>
+          </div>
+          </div>
+          </div>
+    </div>
+            
         </div>
     );
 };
