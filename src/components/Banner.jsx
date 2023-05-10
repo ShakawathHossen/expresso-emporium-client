17f89c9dc8cc3bdcf4eb5d2ea65f694a445a7801
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-05-11 01:07:54 +0600

    delete data works

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,15 +6,13 @@ const Banner = () => {
     const coffees= useLoaderData();
     return (
         <div>
-            Banner
-            <h2>New Added Coffee: {coffees.length}</h2>
             {/* map all coffes data  */}
             <div className='container mx-auto'>
             <div className='text-center'>
             <h1 className='text-3xl font-[Rancho]'>Our Popular Coffee</h1>
             <button>Add Coffee</button>
             </div>
-            <div className="grid md:grid-cols-2 gap-4 ">
+            <div className="grid md:grid-cols-2 gap-8 ">
             {
                 coffees.map((coffee) =><PopularProduct key={coffee._id} coffee={coffee}></PopularProduct>)
             }
--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -1,38 +1,79 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
-const PopularProduct = ({coffee}) => {
+const PopularProduct = ({ coffee }) => {
+    const { _id, photo, name, chef, category } = coffee;
+
+    const hanleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'want to DELETE this Coffee?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+                console.log("delete confirmed");
+                fetch(`http://localhost:5000/coffee/${_id}`,{
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Coffee has been deleted.',
+                                'success'
+                              )
+                        }
+                    });
+
+            }
+        })
+
+    }
+
+
+
+
+
     return (
         <div>
             <div >
-      <div className="bg-[#F4F3F0] p-4 shadow-md rounded-lg ">
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-1">
-            <img
-              src={coffee.photo}
-              alt="Card Image"
-              className="w-full h-auto rounded"
-            />
-          </div>
-          <div className="col-span-1 flex flex-col items-center justify-center">
-            <p className="text-lg font-semibold">Name: {coffee.name}</p>
-            <p className="text-lg font-semibold">Chef: {coffee.chef}</p>
-            <p className="text-lg font-semibold">Category: {coffee.category}</p>
-          </div>
-          <div className="col-span-1 flex flex-col justify-center">
-            <button className="py-2 px-4 mb-2">
-             <img src="https://i.ibb.co/n1qkQJP/view-Button.png" alt="" />
-            </button>
-            <button className="py-2 px-4 mb-2">
-              <img src="https://i.ibb.co/G08dvDK/edit-button.png" alt="" />
-            </button>
-            <button className="py-2 px-4">
-              <img src="https://i.ibb.co/b1ngMDt/delete-button.png" alt="" />
-            </button>
-          </div>
-          </div>
-          </div>
-    </div>
-            
+                <div className="bg-[#F4F3F0] p-4 shadow-md rounded-lg ">
+                    <div className="grid grid-cols-3 gap-4 justify-items-end">
+                        <div className="col-span-1">
+                            <img
+                                src={photo}
+                                alt="Card Image"
+                                className="w-full h-auto rounded"
+                            />
+                        </div>
+                        <div className="col-span-1 flex flex-col items-center justify-center">
+                            <p className="text-lg font-semibold">Name: {name}</p>
+                            <p className="text-lg font-semibold">Chef: {chef}</p>
+                            <p className="text-lg font-semibold">Category: {category}</p>
+                        </div>
+                        <div className="col-span-1 flex flex-col justify-center">
+                            <button className="py-2 px-4 mb-2">
+                                <img src="https://i.ibb.co/n1qkQJP/view-Button.png" alt="" />
+                            </button>
+                            <button className="py-2 px-4 mb-2">
+                                <img src="https://i.ibb.co/G08dvDK/edit-button.png" alt="" />
+                            </button>
+                            <button onClick={() => hanleDelete(_id)} className="py-2 px-4">
+                                <img src="https://i.ibb.co/b1ngMDt/delete-button.png" alt="" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
